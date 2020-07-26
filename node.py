6f257a7c679de4e6395688362e30721eac30b9fe
Marcos De Faria Perez <mdfp1211@gmail.com>
2020-07-27 01:41:21 +0200

    Node class completed

--- node.py
+++ node.py
@@ -1,16 +1,22 @@
+from math import sqrt
 
 class Node:
 
-    def __init__(self, index, x, y, parent, children):
+    def __init__(self, index, x, y, parent = None):
         self.index = index          #Node number
         self.x = x                  #X coordinate
         self.y = y                  #Y coordinate
         self.parent = parent        #Shortest path parent
-        self.children = children    #Connected children Nodes
-        self.f = self.g + self.h    #Total cost of the node
-        self.g = get_g()            #Distance to the start node
-        self.h = get_h()            #Estimated distance to end node
+        self.f = 0                  #Total cost of the node
+        self.g = 0                  #Distance to the start node
+        self.h = 0                  #Estimated distance to end node
 
-    #This method will calculate the distance to the start node
-    def get_g(distance):
-        f = 
+    #This method will calculate the g value
+    def get_g(self):
+        return self.parent.g + sqrt((self.x - self.parent.x)**2 + (self.y - self.parent.y)**2)
+
+    def get_h(self, end):
+        return sqrt((self.x - self.y)**2 + (end.x - end.y)**2)
+
+    def get_f(self):
+        return self.g + self.h
